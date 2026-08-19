22d063e4f1ee0facdeb37a49724c620d1927a453
Matheus Vilarim <matheusvilarim18@gmail.com>
2026-08-19 20:57:13 -0300

    Impementando calculo da posição final

---
+++ ProvaGithub.dart
@@ -0,0 +1,19 @@
+import "dart:io";
+
+void main() {
+  print("Escreva a posição inicial do veívulo em metros: ");
+  int? PosicaoInicial = int.tryParse(stdin.readLineSync() as String);
+
+  print("Digite a velocidade inicial do veículo em metro por segundo: ");
+  int? VelocidadeInicial = int.tryParse(stdin.readLineSync() as String);
+
+  print("Digite a aceleração em metros por segundo: ");
+  int? Aceleracao = int.tryParse(stdin.readLineSync() as String);
+
+  print("Digite o tempo do movimento por segundo: ");
+  int Tempo = int.parse(stdin.readLineSync() as String);
+
+  double PosicaoFinal = PosicaoInicial! +
+      (VelocidadeInicial! * Tempo) +
+      Aceleracao! * (Tempo * Tempo) / 2;
+}
