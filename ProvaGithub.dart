2711199a89b804e3135aa7dece1c5c67409dd08f
Matheus Vilarim <matheusvilarim18@gmail.com>
2026-08-19 21:15:49 -0300

    Trocando TrypParse por Parse

--- ProvaGithub.dart
+++ ProvaGithub.dart
@@ -2,20 +2,20 @@ import "dart:io";
 
 void main() {
   print("Escreva a posição inicial do veívulo em metros: ");
-  int? PosicaoInicial = int.tryParse(stdin.readLineSync() as String);
+  int? PosicaoInicial = int.parse(stdin.readLineSync() as String);
 
   print("Digite a velocidade inicial do veículo em metro por segundo: ");
-  int? VelocidadeInicial = int.tryParse(stdin.readLineSync() as String);
+  int? VelocidadeInicial = int.parse(stdin.readLineSync() as String);
 
   print("Digite a aceleração em metros por segundo: ");
-  int? Aceleracao = int.tryParse(stdin.readLineSync() as String);
+  int? Aceleracao = int.parse(stdin.readLineSync() as String);
 
   print("Digite o tempo do movimento por segundo: ");
   int Tempo = int.parse(stdin.readLineSync() as String);
 
-  double PosicaoFinal = PosicaoInicial! +
-      (VelocidadeInicial! * Tempo) +
-      Aceleracao! * (Tempo * Tempo) / 2;
+  double PosicaoFinal = PosicaoInicial +
+      (VelocidadeInicial * Tempo) +
+      Aceleracao * (Tempo * Tempo) / 2;
 
-  print("A sua posição final é: ${PosicaoFinal}");
+  print("A sua posição final é: ${PosicaoFinal} metros");
 }
